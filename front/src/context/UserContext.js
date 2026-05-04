b329baf066627a713922283214fd44493e54c1f0
julioooo07 <juliusnunez02@gmail.com>
2026-05-05 01:26:45 +0800

    fix web

--- front/src/context/UserContext.js
+++ front/src/context/UserContext.js
@@ -280,8 +280,6 @@ export const UserProvider = ({ children }) => {
     loginPromptMessage,
     register,
     login,
-    loginAsAdmin,
-    loginAsSuperAdmin,
     logout,
     updateProfile,
     updatePreferences,
